2146c4b5a291c38f4145902148fc3efbf0956df6
william <wulixu.william@hotmail.com>
2016-05-25 13:14:10 +0800

    fix qrcode loading bug.

--- Hooters.ClientSDK/Contract/OrderInfo.cs
+++ Hooters.ClientSDK/Contract/OrderInfo.cs
@@ -10,12 +10,14 @@ namespace Hooters.ClientSDK.Contract
     {
         public DateTime CollectingTime { get; set; }
 
-        public long Total { get; set; }
+        public double Total { get; set; }
 
         public int BuyerAge { get; set; }
 
         public string BuyerGender { get; set; }
 
         public string Details { get; set; }
+
+        public string PosOrderId { get; set; }
     }
 }
