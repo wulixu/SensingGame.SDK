a081c1899d7b8b1f2c8a78d26d7450a2bd717ba3
CurryHan <392463168@qq.com>
2016-11-09 15:01:10 +0800

    SensingStore SDK and ClientTest

---
+++ SensingStore.ClientSDK/Contract/DeviceResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensingStore.ClientSDK.Contract
+{
+    public class DeviceResult
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public DeviceInfo Data { get; set; }
+    }
+
+    public class DeviceInfo
+    {
+        public int Id { get; set; }
+        public string Mac { get; set; }
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public string IP { get; set; }
+
+        public DeviceStatus DeviceStatus { get; set; }
+
+    }
+
+    public enum DeviceStatus
+    {
+        Running,
+        Stopped
+    }
+}
---
+++ SensingStore.ClientSDK/Contract/ProductResult.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensingStore.ClientSDK.Contract
+{
+    public class ProductResult
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public List<GroupInfo> Data { get; set; }
+    }
+
+    public class GroupInfo
+    {
+        public string SellerId { get; set; }
+        public string StoreId { get; set; }
+        public List<ProductInfo> Products { get; set; }
+    }
+
+    public class ProductInfo
+    {
+        public int Id { get; set; }
+        public string ItemId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Pic_url { get; set; }
+        public List<ProductImage> ProductImages { get; set; }
+    }
+
+    public class ProductImage
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageSizeType { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
