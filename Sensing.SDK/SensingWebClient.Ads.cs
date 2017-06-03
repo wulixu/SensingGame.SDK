22e40d46e3ff0cf0625f9f1f8cae5700ac08fc6a
william <wulixu.william@hotmail.com>
2017-06-03 18:45:02 +0800

    add device ads apis.

---
+++ Sensing.SDK/SensingWebClient.Ads.cs
@@ -0,0 +1,39 @@
+﻿using Sensing.SDK.Contract;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sensing.SDK
+{
+    partial class SensingWebClient
+    {
+        /// <summary>
+        /// Get all the things.
+        /// </summary>
+        private const string AdsBaseUrl = "StoreSdk";
+        private const string GetAdsQuery = AdsBaseUrl + "/Ads";
+
+        public async Task<List<DeviceAdsViewModel>> GetAds(int page = 1,int maxCount=300)
+        {
+            var absolutePath = $"{ServiceHost}/{GetAdsQuery}?{GetBasicNameValuesQueryString()}&pageSize={maxCount}&page={page}";
+            try
+            {
+                var webResult = await SendRequestAsync<string,WebApiResult<List<DeviceAdsViewModel>>>(HttpMethod.Get, absolutePath,null);
+                if(webResult.status == ApiStatus.OK)
+                {
+                    return webResult.data;
+                }
+                Console.WriteLine("GetAds:" + webResult.message);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                //logger.Error("PostBehaviorRecordsAsync", ex);
+                Console.WriteLine("GetAds:" + ex.InnerException);
+            }
+            return null;
+        }
+    }
+}
