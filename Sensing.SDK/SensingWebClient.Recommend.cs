6d53909d7690609ef80dd4d4798acc0278f8ce35
William Wu <wulixu.william@hotmail.com>
2018-06-21 20:55:47 +0800

    add recommends.

---
+++ Sensing.SDK/SensingWebClient.Recommend.cs
@@ -0,0 +1,14 @@
+﻿using Sensing.SDK.Contract;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sensing.SDK
+{
+    partial class SensingWebClient
+    {
+
+    }
+}
