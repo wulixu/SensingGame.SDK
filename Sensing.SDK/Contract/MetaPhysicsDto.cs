e003e6a07570f1ffaf0a9c06c34f8fb8dd3c9873
William Wu <wulixu.william@hotmail.com>
2018-06-26 20:31:35 +0800

    add metaphysics dto.

---
+++ Sensing.SDK/Contract/MetaPhysicsDto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sensing.SDK.Contract
+{
+    public class MetaPhysicsDto 
+    {
+        public long Id { get; set; }
+        public int? TenantId { get; set; }
+
+        /// <summary>
+        /// Zodiac,Astro
+        /// </summary>
+        public long TypeId { get; set; }
+
+        public MetaphysicsTypeDto Type { get; set; }
+
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 别名
+        /// </summary>
+        public string AliasName { get; set; }
+
+        /// <summary>
+        /// Astro, 取月和日，年忽略.
+        /// Zodiac,暂时忽略.
+        /// </summary>
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public string LogoUrl { get; set; }
+        /// <summary>
+        /// 对应玄学的图片.
+        /// </summary>
+        public string PicUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public string From { get; set; }
+        public string FromLogoUrl { get; set; }
+        public string ThemeColor { get; set; }
+        public string OuterId { get; set; }
+    }
+
+    public class MetaphysicsTypeDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int? TenantId { get; set; }
+        public string Description { get; set; }
+    }
+}
--- Sensing.SDK/SensingWebClient.Recommend.cs
+++ Sensing.SDK/SensingWebClient.Recommend.cs
@@ -9,6 +9,52 @@ namespace Sensing.SDK
 {
     partial class SensingWebClient
     {
+        /// <summary>
+        /// Post device data record.
+        /// </summary>
+        private const string GetDateMetaPhysicsQuery = "/DateMetaPhysics/GetDateMetaphysicsBySubkey";
+        private const string GetMetaphysicsListQuery = "/MetaPhysics/GetMetaphysicsListBySubkey";
+
+        public readonly static string RecommendServiceRelativePath = "r/";
+        public readonly static string RecommendServiceApiHost = ServerBase + DeviceBigDataServiceRelativePath + Api_Relative_Path;
+        public readonly static string RecommendServiceHost = ServerBase + DeviceBigDataServiceRelativePath;
+
+        private const string RecommendPath = "r/api/services/app";
+
+        public async Task<bool> GetDateMetaPhysics(IEnumerable<BehaviorRecord> records)
+        {
+            //api/services/app/BehaviorRecord/PostRecord
+            var absolutePath = $"{ServerBase}{RecommendPath}{GetDateMetaPhysicsQuery}?{GetBasicNameValuesQueryString()}";
+            try
+            {
+                var result = await SendRequestAsync<IEnumerable<BehaviorRecord>, AjaxResponse<bool>>(HttpMethod.Post, absolutePath, records);
+                return result.Success;
+            }
+            catch (Exception ex)
+            {
+                //logger.Error("PostBehaviorRecordsAsync", ex);
+                Console.WriteLine("PostBehaviorRecordsAsync:" + ex.InnerException);
+            }
+            return false;
+        }
+
+
+        public async Task<bool> GetMetaphysicsList(long? typeId)
+        {
+            //api/services/app/BehaviorRecord/PostRecord
+            var absolutePath = $"{ServerBase}{RecommendPath}{GetMetaphysicsListQuery}?{GetBasicNameValuesQueryString()}&typeId={typeId}";
+            try
+            {
+                var result = await SendRequestAsync<IEnumerable<BehaviorRecord>, AjaxResponse<bool>>(HttpMethod.Get, absolutePath, null);
+                return result.Success;
+            }
+            catch (Exception ex)
+            {
+                //logger.Error("PostBehaviorRecordsAsync", ex);
+                Console.WriteLine("GetMetaphysicsList:" + ex.InnerException);
+            }
+            return false;
+        }
 
     }
 }
