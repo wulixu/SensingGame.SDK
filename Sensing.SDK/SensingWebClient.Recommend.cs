d31f7f8199eaa9ec9cb94d67fb5200e42572bcfc
William Wu <wulixu.william@hotmail.com>
2018-06-28 15:03:32 +0800

    fix bugs.

--- Sensing.SDK/SensingWebClient.Recommend.cs
+++ Sensing.SDK/SensingWebClient.Recommend.cs
@@ -23,8 +23,9 @@ namespace Sensing.SDK
 
         public async Task<PagedResultDto<DateMetaphysicsDto>> GetDateMetaPhysics(DateTime startTime, DateTime endTime,long? typeId = null, int skipCount = 0, int maxCount = 300)
         {
+            var typestring = typeId.HasValue ? typeId.Value.ToString() : "";
             //api/services/app/BehaviorRecord/PostRecord
-            var absolutePath = $"{ServerBase}{RecommendPath}{GetDateMetaPhysicsQuery}?{GetBasicNameValuesQueryString()}&typeId={typeId}&startTime={startTime}&endTime={endTime}";
+            var absolutePath = $"{ServerBase}{RecommendPath}{GetDateMetaPhysicsQuery}?{GetBasicNameValuesQueryString()}&typeId={typestring}&startTime={startTime}&endTime={endTime}&{MaxResultCount}={maxCount}&{SkipCount}={skipCount}";
             try
             {
                 var result = await SendRequestAsync<string, AjaxResponse<PagedResultDto<DateMetaphysicsDto>>>(HttpMethod.Get, absolutePath, null);
@@ -41,8 +42,8 @@ namespace Sensing.SDK
 
         public async Task<PagedResultDto<MetaPhysicsDto>> GetMetaphysicsList(long? typeId = null, int skipCount = 0, int maxCount = 300)
         {
-            //api/services/app/BehaviorRecord/PostRecord
-            var absolutePath = $"{ServerBase}{RecommendPath}{GetMetaphysicsListQuery}?{GetBasicNameValuesQueryString()}&typeId={typeId}";
+            var typestring = typeId.HasValue ? typeId.Value.ToString() : "";
+            var absolutePath = $"{ServerBase}{RecommendPath}{GetMetaphysicsListQuery}?{GetBasicNameValuesQueryString()}&typeId={typestring}&{MaxResultCount}={maxCount}&{SkipCount}={skipCount}";
             try
             {
                 var result = await SendRequestAsync<string, AjaxResponse<PagedResultDto<MetaPhysicsDto>>>(HttpMethod.Get, absolutePath, null);
