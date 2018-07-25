d4af73d012b4f881949be2f973d95aba089f12d6
quleo <quleo@DESKTOP-31PMPPQ>
2018-07-25 16:46:04 +0800

    add tenanetId

--- Sensing.SDK/SensingWebClient.cs
+++ Sensing.SDK/SensingWebClient.cs
@@ -132,7 +132,7 @@ namespace Sensing.SDK
                     }
                 }
 
-                response.EnsureSuccessStatusCode();
+                    response.EnsureSuccessStatusCode();
             }
 
             return default(TResponse);
